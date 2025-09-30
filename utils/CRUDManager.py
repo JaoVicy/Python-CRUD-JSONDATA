6fd7fefa4547a33596ef6ceda476d50f7ad9840c
JaoVicy <filipimjoa@gmail.com>
2025-09-30 18:50:24 -0300

    feat: created _generate_new_id()

--- utils/CRUDManager.py
+++ utils/CRUDManager.py
@@ -19,3 +19,11 @@ class CRUDManager:
     def _save_and_persist(self) -> None:
         """Private method to save the list of records to the JSON file."""
         FileManager.save_data(self.records, self.DATA_FILE)
+
+    def _generate_new_id(self) -> int:
+        """Generates a unique ID (the highest existing ID + 1)."""
+        if not self.records:
+            return 1
+        # Get the maximum ID from all records and add 1
+        max_id = max(record.get('id', 0) for record in self.records)
+        return max_id + 1
